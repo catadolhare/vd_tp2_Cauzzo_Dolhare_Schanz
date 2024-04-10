0b26511bf669ea3f137b2c28d210cb42c681bff9
CatalinaDolhare <cdolhare@udesa.edu.ar>
2024-04-10 11:15:12 -0300

    cargado de datos con d3

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,16 @@
 <script>
   import * as d3 from "d3"
+    import { onMount } from "svelte";
 
+  let datos = []
+
+  onMount(() => {
+    d3.csv('./datos_redes.csv', d3.autoType)
+      .then(data => {
+        datos = data
+        console.log(datos)
+    })
+  })
 
 
 </script>
