71404d7c789c7d5373490a4c178577d696176fca
Juana Copello <copellojuana@gmail.com>
2024-04-17 19:31:50 -0300

    solucion problema escalas

--- src/App.svelte
+++ src/App.svelte
@@ -1,68 +1,71 @@
 <script>
-  import * as d3 from "d3"
+  import * as d3 from "d3";
   import { onMount } from "svelte";
 
-  let datos = []
+  let datos = [];
+  //Armo un array con las rutas de los SVG
+  let rutasImagenes = [
+      "./images/circulo.svg",
+      "./images/triangulo.svg",
+      "./images/cuadrado.svg",
+      "./images/pentagono.svg",
+      "./images/hexagono.svg",
+    ];
 
   let aplicaciones = d3
     .scaleOrdinal()
     .domain(["Facebook", "Tiktok", "Instagram", "Twitter", "Otros"])
-    .range(["#14ff00", "#ff0000", "#cc00ff", "#0500ff", "#ffd600"])
+    .range(["#14ff00", "#ff0000", "#cc00ff", "#0500ff", "#ffd600"]);
 
-  let margen_left = d3.scaleLinear()
-    .domain([0, 5])
-    .range([0, 100])
+  let margen_left = d3.scaleLinear().domain([0, 5]).range([0, 100]);
 
-  let margen_bottom = d3.scaleLinear()
-    .domain([0, 5])
-    .range([0, 100])
+  let margen_bottom = d3.scaleLinear().domain([0, 5]).range([0, 100]);
 
-
-
-  let horas_celular = d3.scaleQuantize()
+  let horas_celular = d3
+    .scaleLinear()
     .domain([2, 3, 4, 5, 6])
-    .range(["./images/circulo.svg", "./images/triangulo.svg", "./images/cuadrado.svg", "./images/pentagono.svg", "./images/hexagono.svg"]);
+    .range([0, 1, 2, 3, 4]); // Valores numéricos que representan el índice de cada elemento de rutasImagenes
+
+  /* 
+
+  Armo una función que me retorna 
+  la ruta si yo le paso el índice
+
+  */
+  
+  function getImagePath(indexImage) {
+    return rutasImagenes[indexImage];
+  }
 
   onMount(() => {
-    d3.csv('./datos_redes.csv', d3.autoType)
-      .then(data => {
-        datos = data
-        console.log(datos)
-    })
-  })
-  
+    d3.csv("./datos_redes.csv", d3.autoType).then((data) => {
+      datos = data;
+      console.log(datos);
+    });
+  });
 </script>
 
 <main>
-
-
-<div class="grilla">
-  <div class="quadrant">
-    <div class="ejemplo-alumno">
-      <img src={horas_celular(2)} alt="circulo" />
+  <div class="grilla">
+    <div class="quadrant">
+      <div class="ejemplo-alumno">
+        <!-- Busca la ruta de la imagen a partir 
+        del numero que le retorna horas_celular, 
+        que es el índice del elemento en rutasImagenes -->
+        <img src={getImagePath(horas_celular(2))} alt="circulo" />
+      </div>
     </div>
+    <div class="quadrant"></div>
+    <div class="quadrant"></div>
+    <div class="quadrant"></div>
   </div>
-  <div class="quadrant">
-
-  </div>
-  <div class="quadrant">
-
-  </div>
-  <div class="quadrant">
-
-  </div>
-</div>
-
-
-
 </main>
 
 <style>
-
-.grilla {
+  .grilla {
     display: grid;
     grid-template-columns: 1fr 1fr;
-    height: 100vh; 
+    height: 100vh;
   }
   .quadrant {
     width: 45vw;
@@ -70,12 +73,12 @@
     border: 1px solid #000;
     position: relative;
   }
-  .ejemplo-alumno{
+  .ejemplo-alumno {
     width: 100px;
     height: 100px;
     background-color: red;
     position: absolute;
-    top:50%;
+    top: 50%;
     left: 30%;
   }
 </style>
