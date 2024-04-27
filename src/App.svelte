48241131be37985b8e4a6af627ccfff0617f1c18
CatalinaDolhare <cdolhare@udesa.edu.ar>
2024-04-26 23:03:07 -0300

    agregar simbolo de genero

--- src/App.svelte
+++ src/App.svelte
@@ -36,14 +36,16 @@
     .domain(["Facebook", "Tiktok", "Instagram", "Twitter", "Otros"])
     .range(["#14ff00", "#ff0000", "#cc00ff", "#0500ff", "#ffd600"]);
 
-  let margen_left = d3.scaleLinear().domain([0, 5]).range([0, 100]);
-
-  let margen_bottom = d3.scaleLinear().domain([0, 5]).range([0, 100]);
-
   let horas_celular = d3
     .scaleLinear()
     .domain([2, 3, 4, 5, 6])
     .range([0, 1, 2, 3, 4]); // Valores numéricos que representan el índice de cada elemento de rutasImagenes
+
+  let genero = d3
+    .scaleOrdinal()
+    .domain(["Femenino", "Masculino", "Otro"])
+    .range(["./images/fem.svg", "./images/masc.svg", "./images/otro.svg",]);
+
 
   /* 
 
@@ -115,74 +117,32 @@
   <div class="container">
     {#each datos as alumno}
       <div class="alumno-general">
-        <div class="figura">
-          {@html generarSVG(alumno)}
+        <div class="horas-genero">
+          <div class="figura">
+            {@html generarSVG(alumno)}
+          </div>
+          <div class="genero">
+            <img src="{genero(alumno.genero)}" alt="">
+          </div>
+
         </div>
         <div class="usuario">
           <p>{alumno.nombre}</p>
         </div>
       </div>
     {/each}
-    
-
-  <div class="explicacion">
-    <div class="todos-simbolos">
-      <h2>Símbolos</h2>
-      <p>Los simbolos representan la cantidad de horas utilizando el celular</p>
-      <div class="cont-simbolos">
-        <div class="simbolo">
-          <img src="./images/circulo.svg" alt="" class="color-figure"  />
-          <p>2 horas</p>
-        </div>
-        <div class="simbolo">
-          <img src="./images/triangulo.svg" alt="" class="color-figure" />
-          <p>3 horas</p>
-        </div>
-        <div class="simbolo">
-          <img src="./images/cuadrado.svg" alt="" class="color-figure" />
-          <p>4 horas</p>
-        </div>
-        <div class="simbolo">
-          <img src="./images/pentagono.svg" alt="" class="color-figure" />
-          <p>5 horas</p>
-        </div>
-        <div class="simbolo">
-          <img src="./images/hexagono.svg" alt="" class="color-figure" />
-          <p>6 horas</p>
-        </div>
-      </div>
-    </div>
-    <div class="aplicaciones">
-      <h2>Aplicaciones</h2>
-      <div class="cont-apps-color">
-        <div class="cont-apps">
-          <div class="app"><p>Instagram</p></div>
-            <div class="color" style="background-color: {aplicaciones("Instagram")}"></div>
-        </div>
-        <div class="cont-apps">
-          <div class="app"><p>Facebook</p></div>
-            <div class="color" style="background-color: {aplicaciones("Facebook")}"></div>
-        </div>
-        <div class="cont-apps">
-          <div class="app"><p>Tiktok</p></div>
-            <div class="color" style="background-color: {aplicaciones("Tiktok")}"></div>
-        </div>
-        <div class="cont-apps">
-          <div class="app"><p>Twitter</p></div>
-            <div class="color" style="background-color: {aplicaciones("Twitter")}"></div>
-        </div>
-        <div class="cont-apps">
-          <div class="app"><p>Otros</p></div>
-            <div class="color" style="background-color: {aplicaciones("Otros")}"></div>
-        </div>
-      </div>
-    </div>
   </div>
 </main>
 
 <style>
   main{
     background-color: antiquewhite;
+  }
+  h1{
+    font-family: "Jersey 25", sans-serif;
+    font-weight: 400;
+    font-style: normal;
+    text-align: center;
   }
   .container {
     display: flex;
@@ -196,10 +156,14 @@
   }
   .alumno-general {
     display: flex;
+    flex-direction: column;
     justify-content: center;
     flex-direction: column;
     align-items: center;
     flex: 150px 0 0;
+  }
+  .horas-genero{
+    position: relative;
   }
   .figura{
     width: 120px;
@@ -209,62 +173,16 @@
     width: 100%;
     height: 100%;
   }
-  h1{
-    font-family: "Jersey 25", sans-serif;
-    font-weight: 400;
-    font-style: normal;
-    text-align: center;
+  .genero{
+    width: 20px;
+    height: 20px;
+    position: absolute;
+    top: 23%;
+    left: 42%;
+    font-size: 40px;
+    color: black;
+    font-weight: bold;
   }
-  .explicacion{
-    display: flex;
-    flex-wrap: wrap;
-  }
-  .todos-simbolos{
-    width: 50%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-  .cont-simbolos{
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-  }
-  .simbolo{
-    display: flex;
-    flex-direction: column;
-    margin: 10px;
-    align-items: center;
-  }
-  .aplicaciones{
-    width:50%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
 
-  }
-  .cont-apps-color{
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    width: 200px;
-  }
-  .cont-apps{
-    width: 100%;
-    display: flex;
-    flex-direction: row;
-    
-  }
-  .color{
-    width: 25%;
-    margin-left: 50px;
-    margin-top: 5px;
-    margin-bottom: 5px;
-  }
-  .app{
-    width: 50%;
-  }
-  .color-figure {
-    filter: brightness(0) invert(1) sepia(1) saturate(1000%) hue-rotate(150deg) grayscale(100%);
-  }
+
 </style>
