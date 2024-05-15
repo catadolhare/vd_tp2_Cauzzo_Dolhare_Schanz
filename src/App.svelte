aef0459b513d77b14c40630eef2336490546ce7a
CatalinaDolhare <cdolhare@udesa.edu.ar>
2024-05-15 00:58:53 -0300

    version final

--- src/App.svelte
+++ src/App.svelte
@@ -33,15 +33,6 @@
     }
   }
 
-  //Armo un array con las rutas de los SVG
-  let rutasImagenes = [
-      "./images/circulo.svg",
-      "./images/triangulo.svg",
-      "./images/cuadrado.svg",
-      "./images/pentagono.svg",
-      "./images/hexagono.svg",
-    ];
-
   let genero = d3
     .scaleOrdinal()
     .domain(["Femenino", "Masculino", "Otro"])
@@ -60,7 +51,7 @@
   let posicion_vertical = d3
     .scaleOrdinal()
     .domain(["No", "Si"])
-    .range(["right: 0px", "left: 0px"]);
+    .range(["left: 100%", "left: 0px"]);
 
   /* 
 
@@ -87,27 +78,27 @@
     }
   }
   function obtenerDatosAlumno(alumno) {
-  let datos = `Tiempo de ocio: ${alumno.tiempo_ocio}\n`;
-  datos += `Tiempo de trabajo: ${alumno.tiempo_trabajo}\n`;
-  datos += `Aplicaciones:\n`;
+    let datos = `Tiempo de ocio: ${alumno.tiempo_ocio}\n`;
+    datos += `Tiempo de trabajo: ${alumno.tiempo_trabajo}\n`;
+    datos += `Aplicaciones:\n`;
 
-  if (alumno.tiktok == 1) {
-    datos += `- TikTok\n`;
-  }
-  if (alumno.twitter == 1) {
-    datos += `- Twitter\n`;
-  }
-  if (alumno.instagram == 1) {
-    datos += `- Instagram\n`;
-  }
-  if (alumno.facebook == 1) {
-    datos += `- Facebook\n`;
-  }
-  if (alumno.otros == 1) {
-    datos += `- Otros\n`;
-  }
+    if (alumno.tiktok == 1) {
+      datos += `- TikTok\n`;
+    }
+    if (alumno.twitter == 1) {
+      datos += `- Twitter\n`;
+    }
+    if (alumno.instagram == 1) {
+      datos += `- Instagram\n`;
+    }
+    if (alumno.facebook == 1) {
+      datos += `- Facebook\n`;
+    }
+    if (alumno.otros == 1) {
+      datos += `- Otros\n`;
+    }
 
-  return datos;
+    return datos;
   }
 
   onMount(() => {
@@ -130,9 +121,15 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Jersey+25&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Lilita+One&family=Ubuntu+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
   </head>
   
-  <h1>Uso del celular y redes sociales</h1>
+  <h1>Habitos Digitales</h1>
+  <h2 style="text-align:center; margin: 7px">¿Cuánto usamos nuestro celular? ¿Para que lo usamos? ¿Cómo nos hace sentir?</h2>
+  <p style="text-align:center">Vivimos en un mundo rodeado de tecnología, donde el celular se ha convertido en una extensión de nuestra vida cotidiana. Este proyecto te invita a descubrir de un vistazo cuáles son tus hábitos digitales: qué tanto tiempo que dedicás a tu teléfono, cuáles son tus aplicaciones favoritas, qué impacto emocional tiene en vos...</p>
 
   <div class="container">
     {#each datos as alumno}
@@ -145,10 +142,10 @@
               </div>
             </div>
           </div>
-          <div class="borde-horizontal" style="{posicion_horizontal(alumno)}; ">
+          <div class="borde-horizontal" style="{posicion_horizontal(alumno.perjudica)}; ">
             <img src="{tiempo(alumno.tiempo_ocio)}" alt="" style="width: 100%">
           </div>
-          <div class="borde-vertical" style="{posicion_vertical(alumno)};">
+          <div class="borde-vertical" style="{posicion_vertical(alumno.considera_tiempo)};">
             <img src="{tiempo(alumno.tiempo_trabajo)}" alt="" style="width: 100%">
           </div>
           <div class="genero">
@@ -167,7 +164,7 @@
 
     <div class="explicacion">
       <div class="columna">
-        <p>Tiempo promedio de uso del celular</p>
+        <h2>Tiempo promedio de uso del celular</h2>
         <div class="fotos">
           <div class="foto-container">
             <img src="./images/circulo.svg" style= "background-color: #000000" alt="">
@@ -193,7 +190,7 @@
       </div>
     
       <div class="columna">
-        <p>Genero</p>
+        <h2>Genero</h2>
         <div class="fotos">
           <div class="foto-container">
             <img src="./images/femi.svg" alt="">
@@ -210,10 +207,36 @@
         </div>
       </div>
     </div>
-
+    <div class="explicacion">
+      <div class="columna">
+        <h2>Colores</h2>
+        <div class="fotos">
+          <div class="foto-container">
+            <div class="caja morado"></div>
+            <p>Instagram</p>
+          </div>
+          <div class="foto-container">
+            <div class="caja azul"></div>
+            <p>Twitter</p>
+          </div>
+          <div class="foto-container">
+            <div class="caja rojo"></div>
+            <p>Tik Tok</p>
+          </div>
+          <div class="foto-container">
+            <div class="caja verde"></div>
+            <p>Facebook</p>
+          </div>
+          <div class="foto-container">
+            <div class="caja amarillo"></div>
+            <p>Otros</p>
+          </div>
+        </div>
+      </div>
+    </div>
     <div class="explicacion">  
       <div class="columna">
-        <p>La grilla</p>
+        <h2>La grilla</h2>
         <div>
         <p>La cantidad de lineas representa la cantidad de horas dedicada para el ocio y para el trabajo</p>
         <div class="fotos">
@@ -238,7 +261,7 @@
             <p>5 horas</p>
           </div>
         </div>
-       
+      
 	    <div class="columna">
           <p>La posicion de estas lineas representan la posicion dentro de la grilla</p>
           <div class="fotos">
@@ -261,33 +284,7 @@
           </div>
         </div>
       </div>
-      <div class="explicacion">
-        <div class="columna">
-          <p>Colores</p>
-          <div class="fotos">
-            <div class="foto-container">
-              <div class="caja morado"></div>
-              <p>Instagram</p>
-            </div>
-            <div class="foto-container">
-              <div class="caja azul"></div>
-              <p>Twitter</p>
-            </div>
-            <div class="foto-container">
-              <div class="caja rojo"></div>
-              <p>Tik Tok</p>
-            </div>
-            <div class="foto-container">
-              <div class="caja verde"></div>
-              <p>Facebook</p>
-            </div>
-            <div class="foto-container">
-              <div class="caja amarillo"></div>
-              <p>Otros</p>
-            </div>
-          </div>
-        </div>
-      </div>
+
     </div>
 </main>
 
@@ -301,6 +298,19 @@
     font-weight: 400;
     font-style: normal;
     text-align: center;
+    margin: 20px;
+  }
+  h2{
+    font-family: "Ubuntu Mono", sans-serif;
+    font-optical-sizing: auto;
+    font-weight: 600;
+    font-style: normal;
+  }
+  p{
+    font-family: "Ubuntu Mono", sans-serif;
+    font-optical-sizing: auto;
+    font-weight: 400;
+    font-style: normal;
   }
   .container {
     display: flex;
@@ -323,13 +333,6 @@
   .horas-genero{
     width: 100%;
     position: relative;
-  }
-    .gradiente{
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    top: 0;
-    left: 0;
   }
   .figura{
     width: 100%;
@@ -367,10 +370,10 @@
   }
   .borde-vertical{
     position: absolute;
-    width: 100%; /* Hacemos que el div ocupe todo el ancho del contenedor */
+    width: 100%; 
     bottom: 0;
-    transform-origin: left bottom; /* Establecemos el origen de la rotación */
-    transform: rotate(-90deg); /* Rotamos el div */
+    transform-origin: left bottom; 
+    transform: rotate(-90deg); 
   }
   .info{
     width: 100%;
@@ -420,12 +423,11 @@
     gap: 3px;
   }
   .caja {
-      width: 50px; /* Ancho de cada div */
-      height: 20px; /* Altura de cada div */
-      border-radius: 5px; /* Bordes redondeados */
+      width: 120px;
+      height: 20px; 
+      border-radius: 5px; 
     }
 
-    /* Define los colores de cada div */
     .rojo { background-color: #ff0000; }
     .azul { background-color: #0500ff;}
     .verde { background-color: #14ff00; }
